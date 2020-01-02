e45a2ca9d1a3637f78ccff6e26d24345759abb0e
knzn <rdacles@gmail.com>
2020-01-02 21:21:45 +0800

    2nd Update on Blog page

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,15 @@ module.exports = {
     title: "Robdev Portfolio",
     author: "Roberto Dacles",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    "gatsby-transformer-remark",
+  ],
 }
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,38 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 // Component import
 import Layout from "../components/layout"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
   return (
     <Layout>
-      <h1>This is my blog page</h1>
-      <p>Post will show up here later on.</p>
+      <h1>Blog</h1>
+      <ol>
+        {data.allMarkdownRemark.edges.map(edge => {
+          return (
+            <li>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
